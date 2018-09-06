c5e12c0fb0f556285e8213ee7801885f2c30dd43
Sky <skydjohnson@yahoo.com>
2018-09-06 18:24:12 -0400

    Exercise 1

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    function main (){
+        $("p:grow-me").addClass("big");
+        $("p:shrink-me").remove("big");
+
+        $( "li" ).each()
+          console.log(li.text())
+          
+          $("a").attr("href", "http://www.google.com/")
+
+          $('ul').hide().fadeIn(1000);
+
+
+          $("#hide-me").css("display", "none");
+            $("#show-me").css("display", "block");
+
+          $('#name').attr("Hello Grant");
+    }
+
+    $("#action").on("click", main);
+
+});
